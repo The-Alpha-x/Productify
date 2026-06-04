b74d07bb70321870c81593c7118667a3febcf3a3
Nirmal <nirmal@xoomsoftware.com>
2026-06-04 16:26:54 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware()); //auth obj will be attached to req
+app.use(express.json()); //to handle json data
+app.use(express.urlencoded({ extended: false })); //to handle form data
+
+app.get("/", (req, res) => {
+    res.json({
+        sucess: true,
+        message: "Welcome to Productify API - Powered by PostfresSQL, Drizzle ORM & Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments"
+        }
+    })
+})
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+});
